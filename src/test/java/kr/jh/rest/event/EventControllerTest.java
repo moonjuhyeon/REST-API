319113a50d73d0a2dffb7431694cd39cfe0e2330
moon <hoopster@airi.kr>
2021-09-15 11:29:11 +0900

    feat : use ReqDto and validate unknown params

--- src/test/java/kr/jh/rest/event/EventControllerTest.java
+++ src/test/java/kr/jh/rest/event/EventControllerTest.java
@@ -6,15 +6,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -22,7 +22,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class EventControllerTest {
 
   @Autowired
@@ -31,11 +32,39 @@ public class EventControllerTest {
   @Autowired
   ObjectMapper objectMapper;
 
-  @MockBean
-  EventRepository eventRepository;
-
   @Test
   public void createEvent() throws Exception {
+    // given
+    EventReqDto eventReqDto = EventReqDto.builder()
+        .name("test-name")
+        .description("test-desc")
+        .beginEnrollmentDateTime(LocalDateTime.of(2021, 9, 13, 16,30))
+        .closeEnrollmentDateTime(LocalDateTime.of(2021, 9, 13, 16,50))
+        .beginEventDateTime(LocalDateTime.of(2021, 9, 13, 16,30))
+        .endEventDateTime(LocalDateTime.of(2021, 9, 13, 16,50))
+        .basePrice(100)
+        .maxPrice(200)
+        .limitOfEnrollment(100)
+        .location("jung-ja station")
+        .build();
+
+    // then
+    mockMvc.perform(post("/api/events/")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .accept(MediaTypes.HAL_JSON)
+            .content(objectMapper.writeValueAsString(eventReqDto)))
+        .andDo(print())
+        .andExpect(status().isCreated())
+        .andExpect(jsonPath("id").exists())
+        .andExpect(jsonPath("id").value(Matchers.not(100)))
+        .andExpect(jsonPath("free").value(Matchers.not(true)))
+        .andExpect(header().exists(HttpHeaders.LOCATION))
+        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE))
+        .andExpect(jsonPath("eventStatus").value(EventStatus.DRAFT.toString()));
+  }
+
+  @Test
+  public void createEvent_Bad_Request() throws Exception {
     // given
     Event event = Event.builder()
         .id(100)
@@ -50,23 +79,17 @@ public class EventControllerTest {
         .limitOfEnrollment(100)
         .location("jung-ja station")
         .free(true)
+        .eventStatus(EventStatus.PUBLISHED)
         .build();
 
     // when
-    Mockito.when(eventRepository.save(event)).thenReturn(event);
 
     // then
     mockMvc.perform(post("/api/events/")
-            .contentType(MediaType.APPLICATION_JSON_VALUE)
-            .accept(MediaTypes.HAL_JSON)
-            .content(objectMapper.writeValueAsString(event)))
+        .contentType(MediaType.APPLICATION_JSON_VALUE)
+        .accept(MediaTypes.HAL_JSON)
+        .content(objectMapper.writeValueAsString(event)))
         .andDo(print())
-        .andExpect(status().isCreated())
-        .andExpect(jsonPath("id").exists())
-        .andExpect(jsonPath("id").value(Matchers.not(100)))
-        .andExpect(jsonPath("free").value(Matchers.not(true)))
-        .andExpect(header().exists(HttpHeaders.LOCATION))
-        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaTypes.HAL_JSON_VALUE));
+        .andExpect(status().isBadRequest());
   }
-
 }
