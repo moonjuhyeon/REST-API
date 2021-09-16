c8d6718bfa8454937d3d4b0b12c67f8e673bbe35
moon <hoopster@airi.kr>
2021-09-16 16:30:23 +0900

    refac : parameterized Test refac

--- src/main/java/kr/jh/rest/event/Event.java
+++ src/main/java/kr/jh/rest/event/Event.java
@@ -42,6 +42,6 @@ public class Event {
 
   public void update() {
     this.free = this.basePrice == 0 && this.maxPrice == 0;
-    this.offline = !this.location.isEmpty();
+    this.offline = this.location != null && !this.location.isEmpty();
   }
 }
